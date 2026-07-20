a40cdb9130e43f6fb994f0f78c487edb08153b33
YAKHAIYUMACSE <yakhaiyumacse@gmail.com>
2026-07-20 22:09:26 +0530

    Delete old ProvisionBot folder

--- ProvisionBot/policy/test_policy.py
+++
@@ -1,9 +0,0 @@
-from policy_engine import validate_request
-
-env = input("Enter environment: ")
-infra_type = input("Enter infrastructure type: ")
-
-result, message = validate_request(env, infra_type)
-
-print(result)
-print(message)
